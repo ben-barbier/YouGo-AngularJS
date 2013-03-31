f151eb8b5b93ec28a636942edbca3143d3ceee61
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-03-31 04:04:47 +0200

    implement manageRequestTypesList and manageUserTypesList GUI

--- app/js/app.js
+++ app/js/app.js
@@ -4,11 +4,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/myRequests',         {templateUrl: 'partials/myRequests.html',         controller: MyCtrl1});
-    $routeProvider.when('/newRequest',         {templateUrl: 'partials/newRequest.html',         controller: MyCtrl2});
-    $routeProvider.when('/validateRequests',   {templateUrl: 'partials/validateRequests.html',   controller: MyCtrl2});
-    $routeProvider.when('/manageUsers',        {templateUrl: 'partials/manageUsers.html',        controller: MyCtrl2});
-    $routeProvider.when('/manageUserTypes',    {templateUrl: 'partials/manageUserTypes.html',    controller: MyCtrl2});
-    $routeProvider.when('/manageRequestTypes', {templateUrl: 'partials/manageRequestTypes.html', controller: MyCtrl2});
+    $routeProvider.when('/myRequests',             {templateUrl: 'partials/myRequests.html',             controller: MyCtrl1});
+    $routeProvider.when('/newRequest',             {templateUrl: 'partials/newRequest.html',             controller: MyCtrl2});
+    $routeProvider.when('/validateRequests',       {templateUrl: 'partials/validateRequests.html',       controller: MyCtrl2});
+    $routeProvider.when('/manageUsers',            {templateUrl: 'partials/manageUsers.html',            controller: MyCtrl2});
+    $routeProvider.when('/manageUserTypes',        {templateUrl: 'partials/manageUserTypes.html',        controller: MyCtrl2});
+    $routeProvider.when('/manageUserTypesList',    {templateUrl: 'partials/manageUserTypesList.html',    controller: MyCtrl2});
+    $routeProvider.when('/manageRequestTypes',     {templateUrl: 'partials/manageRequestTypes.html',     controller: MyCtrl2});
+    $routeProvider.when('/manageRequestTypesList', {templateUrl: 'partials/manageRequestTypesList.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/myRequests'});
   }]);
