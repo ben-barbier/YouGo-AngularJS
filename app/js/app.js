ea540e75bc2d9a0a239edd435108b89a78724af9
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-03-31 04:51:27 +0200

    implement manageUsersList GUI

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/newRequest',             {templateUrl: 'partials/newRequest.html',             controller: MyCtrl2});
     $routeProvider.when('/validateRequests',       {templateUrl: 'partials/validateRequests.html',       controller: MyCtrl2});
     $routeProvider.when('/manageUsers',            {templateUrl: 'partials/manageUsers.html',            controller: MyCtrl2});
+    $routeProvider.when('/manageUsersList',        {templateUrl: 'partials/manageUsersList.html',        controller: MyCtrl2});
     $routeProvider.when('/manageUserTypes',        {templateUrl: 'partials/manageUserTypes.html',        controller: MyCtrl2});
     $routeProvider.when('/manageUserTypesList',    {templateUrl: 'partials/manageUserTypesList.html',    controller: MyCtrl2});
     $routeProvider.when('/manageRequestTypes',     {templateUrl: 'partials/manageRequestTypes.html',     controller: MyCtrl2});
