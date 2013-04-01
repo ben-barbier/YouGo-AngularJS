928d60b8a80602918d586002bcbd34285d93e93c
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-04-01 21:02:23 +0200

    put requestTypes in Angular variable

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,12 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/signin',                 {templateUrl: 'partials/signin.html',                 controller: YougoCtrl});
-    $routeProvider.when('/myRequests',             {templateUrl: 'partials/myRequests.html',             controller: YougoCtrl});
-    $routeProvider.when('/newRequest',             {templateUrl: 'partials/newRequest.html',             controller: YougoCtrl});
-    $routeProvider.when('/validateRequests',       {templateUrl: 'partials/validateRequests.html',       controller: YougoCtrl});
-    $routeProvider.when('/manageUsers',            {templateUrl: 'partials/manageUsers.html',            controller: YougoCtrl});
-    $routeProvider.when('/manageUserTypes',        {templateUrl: 'partials/manageUserTypes.html',        controller: YougoCtrl});
-    $routeProvider.when('/manageRequestTypes',     {templateUrl: 'partials/manageRequestTypes.html',     controller: YougoCtrl});
+    $routeProvider.when('/signin',                 {templateUrl: 'partials/signin.html',                 controller: MainCtrl});
+    $routeProvider.when('/myRequests',             {templateUrl: 'partials/myRequests.html',             controller: MainCtrl});
+    $routeProvider.when('/newRequest',             {templateUrl: 'partials/newRequest.html',             controller: MainCtrl});
+    $routeProvider.when('/validateRequests',       {templateUrl: 'partials/validateRequests.html',       controller: MainCtrl});
+    $routeProvider.when('/manageUsers',            {templateUrl: 'partials/manageUsers.html',            controller: MainCtrl});
+    $routeProvider.when('/manageUserTypes',        {templateUrl: 'partials/manageUserTypes.html',        controller: MainCtrl});
+    $routeProvider.when('/manageRequestTypes',     {templateUrl: 'partials/manageRequestTypes.html',     controller: MainCtrl});
     $routeProvider.otherwise({redirectTo: '/signin'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,18 @@
 
 /* Controllers */
 
-function YougoCtrl($scope) {
-	$scope.user = "BOB";
+function MainCtrl($scope) {
+  $scope.user = "BOB";
+
+  $scope.requestTypes = [
+    { id: 1, description: 'Congé payé' },
+    { id: 2, description: 'Congé par anticipation' },
+    { id: 3, description: 'Congé exceptionnel' },
+    { id: 4, description: 'RTT Collaborateur' },
+    { id: 5, description: 'RTT Employeur' },
+    { id: 6, description: 'Récupération' },
+    { id: 7, description: 'Repos compensateur' },
+    { id: 8, description: 'Congé sans solde' }
+  ];
+
 }
