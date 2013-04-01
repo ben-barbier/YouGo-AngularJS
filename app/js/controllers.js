f796b390c3ede64bab0503d3c562abcb1b3409d1
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-04-02 00:00:04 +0200

    put requests in Angular variable

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -29,6 +29,23 @@ function MainCtrl($scope) {
     { id: 3, actif: false, admin: false, nom: 'Sherri MELTON',  email: 'sherri.sherri@company.com',     type: 4 }
   ];
 
+  $scope.requestStatus = [
+    { id: 1, description: 'En attente' },
+    { id: 2, description: 'Validé' },
+    { id: 3, description: 'Refusé'}
+  ];
+
+  $scope.requests = [
+    { id: 1, type: 5, du: '22/10/2010', au: '24/10/2010', observation: 'S‘il vous plait',  reponse: '',             statut: 1},
+    { id: 2, type: 1, du: '23/12/2010', au: '03/01/2011', observation: 'Vacances de Noel', reponse: '',             statut: 1},
+    { id: 3, type: 8, du: '17/02/2011', au: '17/02/2011', observation: '',                 reponse: 'OK',           statut: 2},
+    { id: 4, type: 4, du: '03/01/2011', au: '04/01/2011', observation: '',                 reponse: 'Pas possible', statut: 3}
+  ];
+
+  $scope.selectRequest = function($requestId) {
+    $scope.selectedRequest = $.grep($scope.requests, function(e){ return e.id == $requestId; })[0];
+  };
+
   $scope.selectRequestType = function($requestTypeId) {
     $scope.selectedRequestType = $.grep($scope.requestTypes, function(e){ return e.id == $requestTypeId; })[0];
   };
@@ -43,6 +60,14 @@ function MainCtrl($scope) {
 
   $scope.getUserTypeById = function($userTypeId) {
     return $.grep($scope.userTypes, function(e){ return e.id == $userTypeId; })[0];
+  }
+
+  $scope.getRequestTypeById = function($requestTypeId) {
+    return $.grep($scope.requestTypes, function(e){ return e.id == $requestTypeId; })[0];
+  }
+
+  $scope.getRequestStatusById = function($requestStatusId) {
+    return $.grep($scope.requestStatus, function(e){ return e.id == $requestStatusId; })[0];
   }
 
 }
