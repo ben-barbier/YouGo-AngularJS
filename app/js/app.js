8815bcd2c45dd1aa0e6417481ab8c058398ed367
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-04-01 20:33:16 +0200

    Delete unused pages (xxxList)

--- app/js/app.js
+++ app/js/app.js
@@ -9,10 +9,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/newRequest',             {templateUrl: 'partials/newRequest.html',             controller: YougoCtrl});
     $routeProvider.when('/validateRequests',       {templateUrl: 'partials/validateRequests.html',       controller: YougoCtrl});
     $routeProvider.when('/manageUsers',            {templateUrl: 'partials/manageUsers.html',            controller: YougoCtrl});
-    $routeProvider.when('/manageUsersList',        {templateUrl: 'partials/manageUsersList.html',        controller: YougoCtrl});
     $routeProvider.when('/manageUserTypes',        {templateUrl: 'partials/manageUserTypes.html',        controller: YougoCtrl});
-    $routeProvider.when('/manageUserTypesList',    {templateUrl: 'partials/manageUserTypesList.html',    controller: YougoCtrl});
     $routeProvider.when('/manageRequestTypes',     {templateUrl: 'partials/manageRequestTypes.html',     controller: YougoCtrl});
-    $routeProvider.when('/manageRequestTypesList', {templateUrl: 'partials/manageRequestTypesList.html', controller: YougoCtrl});
     $routeProvider.otherwise({redirectTo: '/signin'});
   }]);
