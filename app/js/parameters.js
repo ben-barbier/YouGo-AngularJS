ae971985428392ea9dd33dbcf38405ab3edb1b28
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-04-15 21:19:21 +0200

    Add Http Header Authorization calculation.

--- app/js/parameters.js
+++ app/js/parameters.js
@@ -1,1 +1,6 @@
 var server = "http://stormy-everglades-6441.herokuapp.com";
+
+//HTTP Header Authorization OK : "Authorization: Basic a3Jpc3RpbmEuY2h1bmdAY29tcGFueS5jb206cGFzc3dvcmQ="
+var HttpBasicLogin = 'kristina.chung@company.com';
+var HttpBasicPassword = 'password';
+var HttpBasicAuthorization = 'Basic ' + Base64.encode(HttpBasicLogin + ':' + HttpBasicPassword);
